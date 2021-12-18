bd29b052d304b9039733c166bd4e7d26e3b3e762
Archibald Perez <archieperez99@gmail.com>
2021-12-18 14:47:45 +0100

    Add hex view for blocks

--- src/components/Block.vue
+++ src/components/Block.vue
@@ -2,29 +2,48 @@
   <div
     class="bordered"
   >
-    {{ block.start }} : {{ block.type }}
-    <button
-      v-if="!block.analysed"
-      type="button"
-      @click="$emit('analyse', block)"
-    >
-      Analyse block
-    </button>
-    <button
-      type="button"
-      @click="showContent = !showContent"
-    >
-      {{ showContent ? 'Hide' : 'Show' }}
-    </button>
-    <textarea
-      v-if="showContent"
-      :value="block.contents"
-      readonly
-    />
+    <div class="flex width-50">
+      {{ block.start }} : {{ block.type }}
+      <div class="flex--align-right">
+        <button
+          v-if="!block.analysed"
+          type="button"
+          @click="$emit('analyse', block)"
+        >
+          Analyse
+        </button>
+        <button
+          type="button"
+          @click="showText = !showText"
+        >
+          {{ showText ? 'Hide text' : 'Show text' }}
+        </button>
+        <button
+          type="button"
+          @click="showHex = !showHex"
+        >
+          {{ showHex ? 'Hide hex' : 'Show hex' }}
+        </button>
+      </div>
+    </div>
+    <div class="flex">
+      <textarea
+        v-if="showText"
+        :value="block.contents"
+        readonly
+      />
+      <textarea
+        v-if="showHex"
+        :value="blockHex"
+        readonly
+      />
+    </div>
   </div>
 </template>
 
 <script>
+import { stringToHexString } from '../util/converters'
+
 export default {
   props: {
     block: Object,
@@ -34,8 +53,15 @@ export default {
 
   data () {
     return {
-      showContent: false,
+      showText: false,
+      showHex: false,
     }
+  },
+
+  computed: {
+    blockHex () {
+      return stringToHexString(this.block.contents)
+    },
   },
 }
 </script>
--- src/util/converters.js
+++ src/util/converters.js
@@ -4,3 +4,11 @@ export function uint8ToString (uint8) {
 export function uint8ToHexString (uint8) {
   uint8.reduce((acc, byte) => acc + (0 + byte.toString(16)).slice(-2), '')
 }
+
+export function stringToHexString (string) {
+  let hex = ''
+  for (const char of string) {
+    hex += `${(0 + char.charCodeAt().toString(16)).slice(-2)}.`
+  }
+  return hex
+}
